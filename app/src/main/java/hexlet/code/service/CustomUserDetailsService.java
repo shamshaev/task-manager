743e19f23920f88d6430d4ad7fbc9059fbe0efe1
shamshaev <an.shamshaev@gmail.com>
2024-08-07 00:26:05 +0300

    Change CustomUserDetailsService

--- app/src/main/java/hexlet/code/service/CustomUserDetailsService.java
+++ app/src/main/java/hexlet/code/service/CustomUserDetailsService.java
@@ -1,13 +1,13 @@
 package hexlet.code.service;
 
-import hexlet.code.model.User;
-import hexlet.code.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.stereotype.Service;
+//import hexlet.code.model.User;
+//import hexlet.code.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.UserDetailsManager;
+//import org.springframework.stereotype.Service;
 
 //@Service
 //public class CustomUserDetailsService implements UserDetailsManager {
