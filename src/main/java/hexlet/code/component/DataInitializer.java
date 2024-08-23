aa719c6bb7969289ede772d52d0878039df69843
shamshaev <an.shamshaev@gmail.com>
2024-08-24 00:13:46 +0300

    Change DataInitializer

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -5,6 +5,7 @@ import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
 import hexlet.code.repository.LabelRepository;
 import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.UserRepository;
 import hexlet.code.service.CustomUserDetailsService;
 import hexlet.code.util.LabelKit;
 import hexlet.code.util.TaskStatusKit;
@@ -15,6 +16,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DataInitializer implements ApplicationRunner {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
@@ -32,18 +36,37 @@ public class DataInitializer implements ApplicationRunner {
     private LabelKit labelKit;
 
     public void run(ApplicationArguments args) {
+
+        if (userRepository.findByEmail("hexlet@example.com").isEmpty()) {
+            initializeAdmin();
+        }
+
+        if (taskStatusRepository.findAll().isEmpty()) {
+            initializeTaskStatuses();
+        }
+
+        if (labelRepository.findAll().isEmpty()) {
+            initializeLabels();
+        }
+    }
+
+    public void initializeAdmin() {
         var userData = new User();
         userData.setEmail("hexlet@example.com");
         userData.setPasswordDigest("qwerty");
         customUserDetailsService.createUser(userData);
+    }
 
+    public void initializeTaskStatuses() {
         for (var slug : taskStatusKit.getSlugs()) {
             var taskStatus = new TaskStatus();
             taskStatus.setSlug(slug);
             taskStatus.setName(taskStatusKit.getName(slug));
             taskStatusRepository.save(taskStatus);
         }
+    }
 
+    public void initializeLabels() {
         for (var labelName : labelKit.getLabelNames()) {
             var label = new Label();
             label.setName(labelName);
